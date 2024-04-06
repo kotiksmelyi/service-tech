6ab7838fb0f4083668a30acda261fa3d82892b95
TigranAvushyan <tigran.avushyan@gmail.com>
2024-04-06 11:55:44 +0300

    feat: aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,21 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import federation from '@originjs/vite-plugin-federation';
-import svgr from "vite-plugin-svgr";
+import svgr from 'vite-plugin-svgr';
+import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@app': path.resolve(__dirname, './src/app'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@features': path.resolve(__dirname, './src/features'),
+      '@widgets': path.resolve(__dirname, './src/widgets'),
+      '@entities': path.resolve(__dirname, './src/entities'),
+      '@shared': path.resolve(__dirname, './src/shared'),
+    },
+  },
   plugins: [
     react(),
     svgr(),
