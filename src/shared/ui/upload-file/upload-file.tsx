306ad1d59a371adfd2ac96f43cc4660ed2ccc5f5
TigranAvushyan <tigran.avushyan@gmail.com>
2024-04-06 15:57:09 +0300

    image

--- src/shared/ui/upload-file/upload-file.tsx
+++ src/shared/ui/upload-file/upload-file.tsx
@@ -1,16 +1,15 @@
 import { Upload, Image, UploadFile as IUploadFile, UploadProps } from 'antd';
-import { FC, Fragment, useState } from 'react';
+import { FC, Fragment, useEffect, useState } from 'react';
 import './upload-file.styles.scss';
 import { RcFile } from 'antd/es/upload';
 
 interface UploadFileProps extends UploadProps {
   buttons?: React.ReactNode[];
-  value?: { file: IUploadFile , fileList: FileList };
+  value?: { file: IUploadFile; fileList: FileList };
 }
 
 const UploadFile: FC<UploadFileProps> = ({ buttons, value, ...props }) => {
   const { Dragger } = Upload;
-  const [previewImage, setPreviewImage] = useState('')
 
   //   const props: UploadProps = {
   //     name: 'file',
@@ -33,46 +32,41 @@ const UploadFile: FC<UploadFileProps> = ({ buttons, value, ...props }) => {
   //   };
 
   const getBase64 = (file: RcFile): Promise<string> =>
-	new Promise((resolve, reject) => {
-		const reader = new FileReader();
-		reader.readAsDataURL(file);
-		reader.onload = () => resolve(reader.result as string);
-		reader.onerror = (error) => reject(error);
-	});
+    new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = () => resolve(reader.result as string);
+      reader.onerror = (error) => reject(error);
+    });
 
+  const [preview, setPreview] = useState('');
   const handlePreview = async (file: any) => {
-    if (!file.url && !file.preview) {
-      file.preview = await getBase64(file.originFileObj as RcFile);
-    }
-    setPreviewImage(file.url || (file.preview as string));
-    // setPreviewOpen(true);
-    // if (file) {
-    //   if (file.name) {
-    //     setPreviewTitle(file.name);
-    //   } else if (file.url) {
-    //     setPreviewTitle(file.url.substring(file.url.lastIndexOf('/') + 1));
-    //   }
-    // }
+    const res = await getBase64(file.originFileObj as RcFile);
+    setPreview(res as string);
   };
 
-  console.log(value);
+  useEffect(() => {
+    if (value) {
+      handlePreview(value.file);
+    }
+  }, [value]);
 
   return (
     <Dragger
       showUploadList={false}
       {...props}
       className="ui-upload"
-      onChange={(e) => console.log(e)}
       maxCount={1}
       accept="image/png, image/jpeg, image/jpg"
     >
       <p className="ant-upload-text">Загрузите фотографию</p>
       <p className="ant-upload-hint">Нажмите или перетащите файл в эту область</p>
-      {
-        value && (
-        <Image src={previewImage} />
-        )
-      }
+      {preview && (
+        <div className="preview" onClick={(e) => e.stopPropagation()}>
+          <Image className="image" width={50} height={50} src={preview} />
+          <span>{value?.file.name}</span>
+        </div>
+      )}
       {buttons && (
         <div className="ant-upload-buttons">
           {buttons?.map((button, index) => (
